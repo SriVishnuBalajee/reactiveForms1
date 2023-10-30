956b09c53fa730443b97cc47b0f9b65bc9cf7124
srivishnu.balaji <srivishnu.balaji@200411LTP2869>
2023-10-30 10:27:41 +0530

    commit 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {FormGroup} from '@angular/forms';
+import {FormControl} from '@angular/forms';
+import {Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactiveForms1';
+  userList:any[]=[];
+  addUser(){
+    if(this.UserData.valid){
+      console.log(this.UserData.value);
+      this.userList.push(this.UserData.value);
+      console.log(this.userList);
+    }
+    else{
+      console.log("invalid");
+    }
+
+  }
+  
+  UserData = new FormGroup({
+    firstname: new FormControl("unknown",[Validators.maxLength(26),Validators.minLength(3),Validators.required]),
+    lastname: new FormControl("unknown",[Validators.maxLength(26),Validators.minLength(3),Validators.required]),
+    salary: new FormControl("unknown",[Validators.max(2600000),Validators.min(3000),Validators.required]),
+    DateofJoin: new FormControl("16-10-2001",[Validators.required]),
+  },
+  {
+    updateOn:'change'
+  }
+  );
 }
